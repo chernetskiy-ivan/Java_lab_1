6c77a4db9eb5c675ee98277286fa7f14558211c1
Ivan <ivanchernetskey@mail.ru>
2020-10-20 15:15:36 +0300

    creat Desert 1.1 (added non-tracked files)

--- src/bsu/rfe/java/group10/lab1/Charnetsky/varC3/Desert.java
+++ src/bsu/rfe/java/group10/lab1/Charnetsky/varC3/Desert.java
@@ -1,4 +1,46 @@
 package bsu.rfe.java.group10.lab1.Charnetsky.varC3;
 
-public class Desert {
+public class Desert extends Food{
+
+    private String Par1;
+    private String Par2;
+
+    public Desert(String name, String Par1, String Par2){
+        super("Десерт");
+        this.Par1 = Par1;
+        this.Par2 = Par2;
+    }
+
+    public String getPar1(){
+        return Par1;
+    }
+
+    public String getPar2(){
+        return Par2;
+    }
+
+    public void setPar1(String InputPar1){
+        this.Par1 = InputPar1;
+    }
+
+    public void setPar2(String InputPar2){
+        this.Par2 = InputPar2;
+    }
+
+    public void consume(){
+        System.out.println(this + " и он весь съеден");
+    }
+
+    public String toString() {
+        return super.toString() + " состоит из " + this.Par1 + " и " + this.Par2;
+    }
+
+    public boolean equals(Object arg0){
+        if (super.equals(arg0)) {
+            if (!(arg0 instanceof Desert)) return false;
+            return (Par1.equals(((Desert)arg0).Par1) && Par2.equals(((Desert)arg0).Par2));
+        } else
+            return false;
+    }
+
 }
