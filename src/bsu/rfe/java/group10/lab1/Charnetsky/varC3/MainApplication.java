96ca90ee093bb5d39756ea82f29f45c14b4699e7
Ivan <ivanchernetskey@mail.ru>
2020-10-21 09:40:10 +0300

    created processing (-calories)

--- src/bsu/rfe/java/group10/lab1/Charnetsky/varC3/MainApplication.java
+++ src/bsu/rfe/java/group10/lab1/Charnetsky/varC3/MainApplication.java
@@ -7,36 +7,49 @@ public class MainApplication {
     public static void main(String[] args) throws Exception {
         Food[] Breakfast = new Food[20];
         int itemsSoFar = 0;
+        boolean case1 = false;
 
-        for (String arg : args){
+        for (String arg : args) {
             String[] parts = arg.split("/");
-            try{
+            try {
                 Class myClass = Class.forName("bsu.rfe.java.group10.lab1.Charnetsky.varC3." + parts[0]);
-                if(parts.length == 1){
+                if (parts.length == 1) {
                     Constructor constructor = myClass.getConstructor();
                     Breakfast[itemsSoFar] = (Food) constructor.newInstance();
                     itemsSoFar++;
                 }
-                if(parts.length == 2){
+                if (parts.length == 2) {
                     Constructor constructor = myClass.getConstructor(String.class);
                     Breakfast[itemsSoFar] = (Food) constructor.newInstance(parts[1]);
                     itemsSoFar++;
                 }
-                if(parts.length == 3){
+                if (parts.length == 3) {
                     Constructor constructor = myClass.getConstructor(String.class, String.class);
-                    Breakfast[itemsSoFar] = (Food) constructor.newInstance(parts[1],parts[2]);
+                    Breakfast[itemsSoFar] = (Food) constructor.newInstance(parts[1], parts[2]);
                     itemsSoFar++;
                 }
-            }
-            catch(ClassNotFoundException e){
-                System.out.println("Класс '" + parts[0] + "' не найден" + e);
-            }
-            catch(NoSuchMethodException eh){
+            } catch (ClassNotFoundException e) {
+                switch (parts[0]) {
+                    case "-calories":
+                        case1 = true;
+                        break;
+                    default:
+                        System.out.println("Класс '" + parts[0] + "' не найден" + e);
+                        break;
+                }
+            } catch (NoSuchMethodException eh) {
                 System.out.println("Метод не найден" + eh);
             }
         }
-        for (int j = 0; j < itemsSoFar;j++) {
-            System.out.println(Breakfast[j]);
+
+        if(case1){
+            Double ALL_calories = 0.0;
+            for(Food item : Breakfast){
+                if(item != null){
+                    ALL_calories += item.CalculateCalories();
+                }
+            }
+            System.out.println("Калорийность завтрака " + ALL_calories);
         }
 
     }
