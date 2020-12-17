c54a05cfeb5e287e0667d839f92da4a4be114e66
Ivan <ivanchernetskey@mail.ru>
2020-12-17 13:45:09 +0300

    added files for solve problem

--- src/bsu/rfe/java/group10/lab1/Charnetsky/varC3/Food.java
+++ src/bsu/rfe/java/group10/lab1/Charnetsky/varC3/Food.java
@@ -1,11 +1,7 @@
 package bsu.rfe.java.group10.lab1.Charnetsky.varC3;
 
 public abstract class Food implements Consumable, Nutritious{
-    //пока не надо
-//    @Override
-//    public void consume(){
-//        // TODO Auto-generated method stub
-//    }
+
     String name = null;
 
     public String getName(){
--- src/bsu/rfe/java/group10/lab1/Charnetsky/varC3/MainApplication.java
+++ src/bsu/rfe/java/group10/lab1/Charnetsky/varC3/MainApplication.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class MainApplication {
-    @SuppressWarnings("unchecked") //Почему он подчеркивает myClass.getConstructor если убрать подавление предупреждений т.е. о чем он меня пытается предупредить?
+
     public static void main(String[] args) throws Exception {
+
         Food[] Breakfast = new Food[20];
         int itemsSoFar = 0;
         boolean case1, case2;
@@ -48,6 +49,57 @@ public class MainApplication {
             }
         }
 
+        Food j;
+        boolean chek = true;
+        int h = 0, i = 0, c = 0;
+        int []a1 = new int[itemsSoFar];
+
+        j = Breakfast[i];
+        for(int k = 0; k < itemsSoFar;k++){
+            if(Breakfast[k].equals(j)){
+                h++;        // Сколько раз встретился этот продукт
+                a1[k] = c; // Первые позиции
+                c++;
+            }
+        }
+        System.out.println(Breakfast[i] + " встретился в завтраке "+ h + " раз.");
+        i++;
+        h = 0;
+
+        do{
+            for(int p = i; p < itemsSoFar; p++) {
+                if (a1[p] != i && a1[p] == 0 ) {
+                    i = p; // Позиция нового не учтенного элемента
+                    break;
+                } else {
+                    chek = false;
+                }
+            }
+            if(chek) {
+                j = Breakfast[i];
+                for (int k = i; k < itemsSoFar; k++) {
+                    if (Breakfast[k].equals(j)) {
+                        h++;
+                        a1[k] = c;
+                    }
+                }
+                System.out.println(Breakfast[i] + " встречается в завтраке " + h + " раз");
+                c++;
+                h = 0;
+            }
+            i++;
+        }while( i < itemsSoFar);
+
+        System.out.println("Завтрак: "); //выводим завтрак таким,каким он был первоначально
+        for (Food item : Breakfast) {
+            if (item != null) {
+                item.consume();
+                System.out.println(" " + item.CalculateCalories());
+            } else {
+                break;
+            }
+        }
+
         if(case1){
             Double ALL_calories = 0.0;
             for(Food item : Breakfast){
@@ -57,7 +109,13 @@ public class MainApplication {
             }
             System.out.println("Калорийность завтрака " + ALL_calories);
         }
+
+
         if (case2) { //случай "ClassNotFoundException", когда мы задаем параметр -sort
+            System.out.println("\n\nДо сортировки");
+            for(int q =0;q < itemsSoFar;q++){
+                System.out.println(Breakfast[q]);
+            }
             Arrays.sort(Breakfast, new Comparator() {
                 public int compare(Object f1, Object f2) {
                     if (f1 == null) return 1;
@@ -71,6 +129,11 @@ public class MainApplication {
                         return 0;
                 }
             });
+
+            System.out.println("\n\nПосле сортировки");
+            for(int q =0;q < itemsSoFar;q++){
+                System.out.println(Breakfast[q]);
+            }
         }
     }
 }
